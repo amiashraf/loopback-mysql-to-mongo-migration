179a3fe1be34b4a8c7673dcd5c778980a0fceb8d
Ashraf Zaman <amiashraf@gmail.com>
2015-11-30 13:46:01 +0600

    commiting election model

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -5,44 +5,70 @@ var ds = app.datasources.mongovotebdtest;
 var oldds = app.datasources.votebdold;
 
 
-oldds.discoverAndBuildModels('Account', {schema: 'votebd_july2015'},
+oldds.discoverAndBuildModels('election_list', {schema: 'votebd_july2015'},
   function(err, models) {
     if (err) throw err;
 
-    models.Account.find(function(err, accounts) {
+    models.ElectionList.find(function(err, elections) {
       if (err) throw err;
 
-      console.log('Found:', accounts);
+      console.log('Found:', elections);
 
-      ds.disconnect();
+
+      var count = elections.length;
+      elections.forEach(function(election) {
+        app.models.Donor.create(election, function(err, model) {
+          if (err) throw err;
+
+          console.log('Created:', model);
+
+          count--;
+          if (count === 0)
+            ds.disconnect();
+        });
+      });
+
+
+
+      oldds.disconnect();
     });
   });
 
-ds.automigrate('Donor', function(err) {
-  if (err) throw err;
 
-  var accounts = [
-    {
-      name: 'kamela'
-    },
-    {
-      name: 'tamela'
-    },
-    {
-      name: 'pamela'
-    }
-  ];
-  var count = accounts.length;
-  accounts.forEach(function(account) {
-    app.models.Donor.create(account, function(err, model) {
-      if (err) throw err;
 
-      console.log('Created:', model);
+//ds.automigrate('Donor', function(err) {
+//  if (err) throw err;
+//
+//  var elections = [
+//    {
+//      name: 'kamela'
+//    },
+//    {
+//      name: 'tamela'
+//    },
+//    {
+//      name: 'pamela'
+//    }
+//  ];
+//  var count = elections.length;
+//  elections.forEach(function(election) {
+//    app.models.Donor.create(election, function(err, model) {
+//      if (err) throw err;
+//
+//      console.log('Created:', model);
+//
+//      count--;
+//      if (count === 0)
+//        ds.disconnect();
+//    });
+//  });
+//
+//});
 
-      count--;
-      if (count === 0)
-        ds.disconnect();
-    });
-  });
 
-});
+//{ id: 17,
+//  electionNameEng: 'Dhaka City Corp (North)',
+//  electionNameBng: 'ঢাকা সিটি করপোরেশন (দক্ষিণ)',
+//  electionDetails: 'সিটি করপোরেশন নির্বাচন',
+//  electionHistory: 'পূর্বে ঢাকা একটি সিটিতেই বিভক্ত ছিল',
+//  electionType: 'CITY-CORP' },
