352af1523443674d41c1789f125981794824c82f
Ashraf Zaman <amiashraf@gmail.com>
2015-12-01 15:46:14 +0600

    added eleciton model and migration script

---
+++ bin/migration_district.js
@@ -0,0 +1,256 @@
+var path = require('path');
+
+var app = require(path.resolve(__dirname, '../server/server'));
+var ds = app.datasources.mongovotebdtest;
+var oldds = app.datasources.votebdold;
+
+
+////Step 1: Showing table structure
+//oldds.discoverSchema('district_list', {schema: 'votebd_july2015'}, function(err,schema) {
+//  if (err) throw err;
+//
+//  var json = JSON.stringify(schema, null, '  ');
+//  console.log(json);
+//
+//  oldds.disconnect();
+//});
+
+////Step 2: Showing data
+//oldds.discoverAndBuildModels('district_list', {schema: 'votebd_july2015'},
+//  function(err, models) {
+//    if (err) throw err;
+//
+//    models.DistrictList.find(function(err, districts) {
+//      if (err) throw err;
+//
+//      console.log('Found:', districts);
+//
+//      oldds.disconnect();
+//    });
+//  });
+
+
+//Step 3: Retreiving data from mysql and push to mongo
+oldds.discoverAndBuildModels('district_list', {schema: 'votebd_july2015'},
+  function(err, models) {
+    if (err) throw err;
+
+    models.DistrictList.find(function(err, districts) {
+      if (err) throw err;
+
+      console.log('Found:', districts);
+
+
+      //importing to mongo
+      var count = districts.length;
+      districts.forEach(function(district) {
+
+        var distr = {};
+        distr.id = district.slNo;
+        distr.nameEn = district.districtName;
+        distr.nameBn = district.districtNameBng;
+        distr.infoBn = district.districtInfo;
+        distr.divisionId = district.divisionId;
+        distr.districtCode = district.districtCode;
+
+
+        app.models.district.create(distr, function(err, model) {
+          if (err) throw err;
+
+          console.log('Created:', model);
+
+          count--;
+          if (count === 0)
+            ds.disconnect();
+        });
+      });
+
+
+
+      oldds.disconnect();
+    });
+  });
+
+//////////////////TABLE SCHEMA//////////////////////////////////////
+//{
+//  "name": "DistrictList",
+//  "options": {
+//  "idInjection": false,
+//    "mysql": {
+//    "schema": "votebd_july2015",
+//      "table": "district_list"
+//  }
+//},
+//  "properties": {
+//  "slNo": {
+//    "type": "Number",
+//      "required": true,
+//      "length": null,
+//      "precision": 10,
+//      "scale": 0,
+//      "id": 1,
+//      "mysql": {
+//      "columnName": "SL_NO",
+//        "dataType": "int",
+//        "dataLength": null,
+//        "dataPrecision": 10,
+//        "dataScale": 0,
+//        "nullable": "N"
+//    }
+//  },
+//  "districtCode": {
+//    "type": "String",
+//      "required": true,
+//      "length": 12,
+//      "precision": null,
+//      "scale": null,
+//      "mysql": {
+//      "columnName": "DISTRICT_CODE",
+//        "dataType": "varchar",
+//        "dataLength": 12,
+//        "dataPrecision": null,
+//        "dataScale": null,
+//        "nullable": "N"
+//    }
+//  },
+//  "divisionId": {
+//    "type": "String",
+//      "required": true,
+//      "length": 4,
+//      "precision": null,
+//      "scale": null,
+//      "mysql": {
+//      "columnName": "DIVISION_ID",
+//        "dataType": "varchar",
+//        "dataLength": 4,
+//        "dataPrecision": null,
+//        "dataScale": null,
+//        "nullable": "N"
+//    }
+//  },
+//  "districtName": {
+//    "type": "String",
+//      "required": true,
+//      "length": 30,
+//      "precision": null,
+//      "scale": null,
+//      "mysql": {
+//      "columnName": "DISTRICT_NAME",
+//        "dataType": "varchar",
+//        "dataLength": 30,
+//        "dataPrecision": null,
+//        "dataScale": null,
+//        "nullable": "N"
+//    }
+//  },
+//  "totalPeople": {
+//    "type": "Number",
+//      "required": true,
+//      "length": null,
+//      "precision": 10,
+//      "scale": 0,
+//      "mysql": {
+//      "columnName": "TOTAL_PEOPLE",
+//        "dataType": "int",
+//        "dataLength": null,
+//        "dataPrecision": 10,
+//        "dataScale": 0,
+//        "nullable": "N"
+//    }
+//  },
+//  "totalVoterMale": {
+//    "type": "Number",
+//      "required": true,
+//      "length": null,
+//      "precision": 10,
+//      "scale": 0,
+//      "mysql": {
+//      "columnName": "TOTAL_VOTER_MALE",
+//        "dataType": "int",
+//        "dataLength": null,
+//        "dataPrecision": 10,
+//        "dataScale": 0,
+//        "nullable": "N"
+//    }
+//  },
+//  "totalVoterFemale": {
+//    "type": "Number",
+//      "required": true,
+//      "length": null,
+//      "precision": 10,
+//      "scale": 0,
+//      "mysql": {
+//      "columnName": "TOTAL_VOTER_FEMALE",
+//        "dataType": "int",
+//        "dataLength": null,
+//        "dataPrecision": 10,
+//        "dataScale": 0,
+//        "nullable": "N"
+//    }
+//  },
+//  "districtNameBng": {
+//    "type": "String",
+//      "required": true,
+//      "length": 255,
+//      "precision": null,
+//      "scale": null,
+//      "mysql": {
+//      "columnName": "DISTRICT_NAME_BNG",
+//        "dataType": "varchar",
+//        "dataLength": 255,
+//        "dataPrecision": null,
+//        "dataScale": null,
+//        "nullable": "N"
+//    }
+//  },
+//  "districtInfo": {
+//    "type": "String",
+//      "required": true,
+//      "length": 4294967295,
+//      "precision": null,
+//      "scale": null,
+//      "mysql": {
+//      "columnName": "DISTRICT_INFO",
+//        "dataType": "longtext",
+//        "dataLength": 4294967295,
+//        "dataPrecision": null,
+//        "dataScale": null,
+//        "nullable": "N"
+//    }
+//  }
+//}
+//}
+//
+
+
+
+
+/////////////////////DATA///////////////////////////////////
+//[ { slNo: 1,
+//  districtCode: '5000',
+//  divisionId: '7',
+//  districtName: 'Panchagarh',
+//  totalPeople: 0,
+//  totalVoterMale: 0,
+//  totalVoterFemale: 0,
+//  districtNameBng: 'পঞ্চগড়',
+//  districtInfo: 'পঞ্চগড় জেলার উত্তরে ভারতের পশ্চিমবঙ্গ, দক্ষিণে ঠাকুরগাঁও ও দিনাজপুর জেলা, পূর্বে নীলফামারী জেলা এবং পশ্চিমে ভারতের পশ্চিমবঙ্গ অবস্থিত।' },
+//  { slNo: 2,
+//    districtCode: '5100',
+//    divisionId: '7',
+//    districtName: 'Thakurgaon',
+//    totalPeople: 0,
+//    totalVoterMale: 0,
+//    totalVoterFemale: 0,
+//    districtNameBng: 'ঠাকুরগাঁও',
+//    districtInfo: 'ঠাকুরগাঁও জেলার উত্তরে পঞ্চগড় জেলা, দক্ষিণে দিনাজপুর জেলা ও ভারতের পশ্চিমবঙ্গ, পূর্বে দিনাজপুর জেলা এবং পশ্চিমে ভারতের পশ্চিমবঙ্গ অবস্থিত।' },
+//  { slNo: 3,
+//    districtCode: '2500',
+//    divisionId: '7',
+//    districtName: 'Dinajpur',
+//    totalPeople: 0,
+//    totalVoterMale: 0,
+//    totalVoterFemale: 0,
+//    districtNameBng: 'দিনাজপুর',
+//    districtInfo: '-' }
+//  ]
--- bin/migration_election.js
+++ bin/migration_election.js
@@ -5,75 +5,74 @@ var ds = app.datasources.mongovotebdtest;
 var oldds = app.datasources.votebdold;
 
 
-oldds.discoverAndBuildModels('election_list', {schema: 'votebd_july2015'},
-  function(err, models) {
-    if (err) throw err;
-
-    models.ElectionList.find(function(err, elections) {
-      if (err) throw err;
-
-      console.log('Found:', elections);
-
-
-      var count = elections.length;
-      elections.forEach(function(election) {
-        app.models.ElectionList.create(election, function(err, model) {
-          if (err) throw err;
-
-          console.log('Created:', model);
-
-          count--;
-          if (count === 0)
-            ds.disconnect();
-        });
-      });
-
-
-
-      oldds.disconnect();
-    });
-  });
-
-
-
-//ds.automigrate('Donor', function(err) {
+////Step 1: Showing table structure
+//oldds.discoverSchema('election_list', {schema: 'votebd_july2015'}, function(err,schema) {
 //  if (err) throw err;
 //
-//  var elections = [
-//    {
-//      name: 'kamela'
-//    },
-//    {
-//      name: 'tamela'
-//    },
-//    {
-//      name: 'pamela'
-//    }
-//  ];
-//  var count = elections.length;
-//  elections.forEach(function(election) {
-//    app.models.Donor.create(election, function(err, model) {
-//      if (err) throw err;
+//  var json = JSON.stringify(schema, null, '  ');
+//  console.log(json);
 //
-//      console.log('Created:', model);
-//
-//      count--;
-//      if (count === 0)
-//        ds.disconnect();
-//    });
-//  });
-//
+//  oldds.disconnect();
 //});
 
-
-//{ id: 17,
-//  electionNameEng: 'Dhaka City Corp (North)',
-//  electionNameBng: 'ঢাকা সিটি করপোরেশন (দক্ষিণ)',
-//  electionDetails: 'সিটি করপোরেশন নির্বাচন',
-//  electionHistory: 'পূর্বে ঢাকা একটি সিটিতেই বিভক্ত ছিল',
-//  electionType: 'CITY-CORP' },
-
+//Step 2: Showing data
+//oldds.discoverAndBuildModels('election_list', {schema: 'votebd_july2015'},
+//  function(err, models) {
+//    if (err) throw err;
 //
+//    models.ElectionList.find(function(err, elections) {
+//      if (err) throw err;
+//
+//      console.log('Found:', elections);
+//
+//      oldds.disconnect();
+//    });
+//  });
+
+
+////Step 3: Retreiving data from mysql and push to mongo
+//oldds.discoverAndBuildModels('election_list', {schema: 'votebd_july2015'},
+//  function(err, models) {
+//    if (err) throw err;
+//
+//    models.ElectionList.find(function(err, elections) {
+//      if (err) throw err;
+//
+//      console.log('Found:', elections);
+//
+//
+//      //importing to mongo
+//      var count = elections.length;
+//      elections.forEach(function(election) {
+//
+//        var divisn = {};
+//        divisn.id = election.electionId;
+//        divisn.nameEn = election.electionNameEng;
+//        divisn.nameBn = election.electionNameBng;
+//        divisn.detail = election.electionDetails;
+//        divisn.history = election.electionHistory;
+//        divisn.electionType = election.electionType;
+//
+//
+//        app.models.election.create(divisn, function(err, model) {
+//          if (err) throw err;
+//
+//          console.log('Created:', model);
+//
+//          count--;
+//          if (count === 0)
+//            ds.disconnect();
+//        });
+//      });
+//
+//
+//
+//      oldds.disconnect();
+//    });
+//  });
+
+
+/////////////////////////////schema////////////////////////
 //{
 //  "name": "ElectionList",
 //  "options": {
@@ -177,4 +176,115 @@ oldds.discoverAndBuildModels('election_list', {schema: 'votebd_july2015'},
 //  }
 //}
 //}
+
+
+//////////////////////////////////data//////////////////
 //
+//[ { id: 1,
+//  electionNameEng: 'Members of parliament election',
+//  electionNameBng: 'জাতীয় সংসদ নির্বাচন',
+//  electionDetails: 'জাতীয় সংসদ নির্বাচন(National members of parliament election)',
+//  electionHistory: '',
+//  electionType: 'MP' },
+//  { id: 2,
+//    electionNameEng: 'By-Election',
+//    electionNameBng: 'উপনির্বাচন ',
+//    electionDetails: 'উপনির্বাচন(By-Election)',
+//    electionHistory: '',
+//    electionType: 'BY-ELECTION' },
+//  { id: 3,
+//    electionNameEng: 'City Corporation Election',
+//    electionNameBng: 'সিটি কর্পোরেশন নির্বাচন ',
+//    electionDetails: 'সিটি কর্পোরেশন নির্বাচন ',
+//    electionHistory: 'সিটি কর্পোরেশন নির্বাচন ',
+//    electionType: 'CITY-CORP' },
+//  { id: 4,
+//    electionNameEng: 'Pourashava Election',
+//    electionNameBng: 'পৌরসভা নির্বাচন',
+//    electionDetails: 'পৌরসভা নির্বাচন',
+//    electionHistory: 'পৌরসভা নির্বাচন',
+//    electionType: 'UNION' },
+//  { id: 5,
+//    electionNameEng: 'Upazila Election',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন',
+//    electionDetails: 'উপজেলা নির্বাচন',
+//    electionHistory: 'উপজেলা নির্বাচন',
+//    electionType: 'UPAZILLA' },
+//  { id: 6,
+//    electionNameEng: 'By Election (Ninth Parliament )',
+//    electionNameBng: 'উপনির্বাচন (নবম জাতীয় সংসদ)',
+//    electionDetails: 'উপনির্বাচন (নবম জাতীয় সংসদ)',
+//    electionHistory: 'উপনির্বাচন (নবম জাতীয় সংসদ)',
+//    electionType: 'MP' },
+//  { id: 7,
+//    electionNameEng: 'Upazila 1st phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ১ম পর্ব ২০১৪',
+//    electionDetails: 'উপজেলা পরিষদ',
+//    electionHistory: 'উপজেলা পরিষদ',
+//    electionType: 'UPAZILLA' },
+//  { id: 8,
+//    electionNameEng: 'Upazila 2nd phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ২য় পর্ব ২০১৪',
+//    electionDetails: 'উপজেলা পরিষদ',
+//    electionHistory: 'উপজেলা পরিষদ',
+//    electionType: 'UPAZILLA' },
+//  { id: 9,
+//    electionNameEng: 'Upazila 3rd phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ৩য় পর্ব',
+//    electionDetails: 'উপজেলা পরিষদ',
+//    electionHistory: 'উপজেলা পরিষদ',
+//    electionType: 'UPAZILLA' },
+//  { id: 10,
+//    electionNameEng: 'Upazila Election 4th Phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ৪র্থ পর্ব',
+//    electionDetails: 'উপজেলা নির্বাচন',
+//    electionHistory: 'ইতিহাস',
+//    electionType: 'UPAZILLA' },
+//  { id: 11,
+//    electionNameEng: 'Reserved Seat',
+//    electionNameBng: 'সংরক্ষিত নারী আসন',
+//    electionDetails: 'নারী আসন',
+//    electionHistory: 'নারী আসন',
+//    electionType: 'MP' },
+//  { id: 12,
+//    electionNameEng: 'Upazila Election 5th Phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ৫ম পর্ব',
+//    electionDetails: 'Here is the election of 5th phase Upazila election',
+//    electionHistory: 'Here is the election of 5th phase Upazila election',
+//    electionType: 'UPAZILLA' },
+//  { id: 13,
+//    electionNameEng: 'U[azila Election 6th Phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ৬ষ্ঠ পর্ব',
+//    electionDetails: 'upazila',
+//    electionHistory: 'ইতিহাস',
+//    electionType: 'UPAZILLA' },
+//  { id: 14,
+//    electionNameEng: 'Pospond Election',
+//    electionNameBng: 'স্থগিত নির্বাচন',
+//    electionDetails: 'যেকোন কারণে পরবর্তীতে অনুষ্ঠিত নির্বাচন',
+//    electionHistory: 'যেকোন কারণে পরবর্তীতে অনুষ্ঠিত নির্বাচন',
+//    electionType: 'UPAZILLA' },
+//  { id: 15,
+//    electionNameEng: 'Upazila Election 7th Phase',
+//    electionNameBng: 'উপজেলা পরিষদ নির্বাচন ৭ম পর্ব',
+//    electionDetails: 'সপ্তম',
+//    electionHistory: 'নির্বাচন',
+//    electionType: 'UPAZILLA' },
+//  { id: 16,
+//    electionNameEng: 'Dhaka City Corp (North)',
+//    electionNameBng: 'ঢাকা সিটি করপোরেশন (উত্তর)',
+//    electionDetails: '২টি সিটি নির্বাচনে বিভক্ত',
+//    electionHistory: 'পূর্বে ঢাকা একটি সিটিতেই বিভক্ত ছিল',
+//    electionType: 'CITY-CORP' },
+//  { id: 17,
+//    electionNameEng: 'Dhaka City Corp (North)',
+//    electionNameBng: 'ঢাকা সিটি করপোরেশন (দক্ষিণ)',
+//    electionDetails: 'সিটি করপোরেশন নির্বাচন',
+//    electionHistory: 'পূর্বে ঢাকা একটি সিটিতেই বিভক্ত ছিল',
+//    electionType: 'CITY-CORP' },
+//  { id: 18,
+//    electionNameEng: 'Chittagong City Corp (North)',
+//    electionNameBng: 'চট্টগ্রাম সিটি করপোরেশন',
+//    electionDetails: 'পূর্বে ঢাকা একটি সিটিতেই বিভক্ত ছিল',
+//    electionHistory: 'পূর্বে ঢাকা একটি সিটিতেই বিভক্ত ছিল',
+//    electionType: 'CITY-CORP' } ]
