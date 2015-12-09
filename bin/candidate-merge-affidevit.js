99c9f9b3c7873e801ac04377eac9e5676d35889b
Ashraf Zaman <amiashraf@gmail.com>
2015-12-09 12:52:36 +0600

    added loan integration and adding income section

--- bin/candidate-merge-affidevit.js
+++ bin/candidate-merge-affidevit.js
@@ -4,6 +4,13 @@ var app = require(path.resolve(__dirname, '../server/server'));
 var ds = app.datasources.votebdmongo;
 var oldds = app.datasources.votebdold;
 
+
+
+function IsNumeric(input)
+{
+  return (input - 0) == input && (''+input).trim().length > 0;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////MERGIN PROFILE ID AS OLDAFFIDAVITID////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -187,8 +194,132 @@ var oldds = app.datasources.votebdold;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////MERGE AFFIDEVIT COURT SECTIION //////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//oldds.connector.query("SELECT profile_id, COUNT(id) as cnt FROM effidevit_court_section group by profile_id limit 6450, 150",'',function(err,rows,fields) {
+//  if(err){
+//    console.log(err);
+//    throw err;
+//  }
+//
+//  console.log(rows.length);
+//  //console.log(rows);
+//
+//  var iter=1;
+//  if(rows.length){
+//    rows.forEach(function(eachRow){
+//      console.log("working with profile_id: ",eachRow.profile_id," length: ",eachRow.cnt);
+//      oldds.connector.query("SELECT * FROM effidevit_court_section where profile_id="+eachRow.profile_id,'',function(err22,rows2,fields) {
+//
+//        if(err22){
+//          console.log("-------------------",err22);
+//          throw err22;
+//        }
+//
+//        var profilesLen = rows2.length;
+//
+//
+//        if(profilesLen){
+//          var present = [];
+//          var past = [];
+//          for(var i=0;i<profilesLen;i++){
+//            var lawCourt = {whichLaw:rows2[i].which_law, whichCourt:rows2[i].which_court, courtFileNo:rows2[i].court_file_no, currentStatusResult:rows2[i].current_status_result};
+//            if(rows2[i].status == "present"){
+//              present.push(lawCourt);
+//            }
+//            if(rows2[i].status == "past"){
+//              past.push(lawCourt);
+//            }
+//          }
+//
+//          var candidateAffidevit={};
+//          candidateAffidevit.lawPresentCountAF = present.length;
+//          if(candidateAffidevit.lawPresentCountAF) {
+//            candidateAffidevit.lawPresentAF = present;
+//          }
+//          candidateAffidevit.lawPastCountAF = past.length;
+//          if(candidateAffidevit.lawPastCountAF )
+//            candidateAffidevit.lawPastAF = past;
+//          //console.log(candidateAffidevit);
+//
+//
+//          app.models.candidate.updateAll({oldAffidevitId: eachRow.profile_id}, candidateAffidevit, function(errr, info) {
+//            if(errr)
+//              throw errr;
+//
+//            console.log(info, iter++);
+//          });
+//        }
+//      });
+//    })
+//  }
+//
+//
+//  //oldds.disconnect();
+//});
 
-oldds.connector.query("SELECT profile_id, COUNT(id) as cnt FROM effidevit_court_section group by profile_id limit 2550, 150",'',function(err,rows,fields) {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////MERGE AFFIDEVIT Loan section //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//oldds.connector.query("SELECT profile_id, COUNT(id) as cnt FROM `effidevit_loans_section` group by profile_id limit 2950, 150",'',function(err,rows,fields) {
+//  if(err){
+//    console.log(err);
+//    throw err;
+//  }
+//
+//  console.log(rows.length);
+//  //console.log(rows);
+//
+//  var iter=1;
+//  if(rows.length){
+//    rows.forEach(function(eachRow){
+//      console.log("working with profile_id: ",eachRow.profile_id," length: ",eachRow.cnt);
+//      oldds.connector.query("SELECT * FROM `effidevit_loans_section` where profile_id="+eachRow.profile_id,'',function(err22,rows2,fields) {
+//
+//        if(err22){
+//          console.log("-------------------",err22);
+//          throw err22;
+//        }
+//
+//        var profilesLen = rows2.length;
+//
+//
+//        if(profilesLen){
+//          var loanAF = [];
+//          var totalLoan=0;
+//          for(var i=0;i<profilesLen;i++){
+//            loanAF.push({loanType:rows2[i].loan_type, bankOrganization:rows2[i].bank_organization, loanAmount:rows2[i].loan_amount, debitedLoanAmount:rows2[i].debited_loan_amount, extendedLastDate:rows2[i].extended_last_date});
+//            if(IsNumeric(rows2[i].loan_amount))
+//              totalLoan+= Number(rows2[i].loan_amount);
+//          }
+//
+//          var candidateAffidevit={};
+//          candidateAffidevit.totalLoanAF = totalLoan;
+//          candidateAffidevit.loanAF = loanAF;
+//
+//          //console.log(candidateAffidevit);
+//
+//          app.models.candidate.updateAll({oldAffidevitId: eachRow.profile_id}, candidateAffidevit, function(errr, info) {
+//            if(errr)
+//              throw errr;
+//
+//            console.log(info, iter++);
+//          });
+//        }
+//      });
+//    })
+//  }
+//
+//
+//  //oldds.disconnect();
+//});
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////MERGE AFFIDEVIT income section //////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+oldds.connector.query("SELECT profile_id, COUNT(id) as cnt FROM `effidevit_loans_section` group by profile_id limit 2950, 150",'',function(err,rows,fields) {
   if(err){
     console.log(err);
     throw err;
@@ -201,7 +332,7 @@ oldds.connector.query("SELECT profile_id, COUNT(id) as cnt FROM effidevit_court_
   if(rows.length){
     rows.forEach(function(eachRow){
       console.log("working with profile_id: ",eachRow.profile_id," length: ",eachRow.cnt);
-      oldds.connector.query("SELECT * FROM effidevit_court_section where profile_id="+eachRow.profile_id,'',function(err22,rows2,fields) {
+      oldds.connector.query("SELECT * FROM `effidevit_loans_section` where profile_id="+eachRow.profile_id,'',function(err22,rows2,fields) {
 
         if(err22){
           console.log("-------------------",err22);
@@ -212,28 +343,19 @@ oldds.connector.query("SELECT profile_id, COUNT(id) as cnt FROM effidevit_court_
 
 
         if(profilesLen){
-          var present = [];
-          var past = [];
+          var loanAF = [];
+          var totalLoan=0;
           for(var i=0;i<profilesLen;i++){
-            var lawCourt = {whichLaw:rows2[i].which_law, whichCourt:rows2[i].which_court, courtFileNo:rows2[i].court_file_no, currentStatusResult:rows2[i].current_status_result};
-            if(rows2[i].status == "present"){
-              present.push(lawCourt);
-            }
-            if(rows2[i].status == "past"){
-              past.push(lawCourt);
-            }
+            loanAF.push({loanType:rows2[i].loan_type, bankOrganization:rows2[i].bank_organization, loanAmount:rows2[i].loan_amount, debitedLoanAmount:rows2[i].debited_loan_amount, extendedLastDate:rows2[i].extended_last_date});
+            if(IsNumeric(rows2[i].loan_amount))
+              totalLoan+= Number(rows2[i].loan_amount);
           }
 
           var candidateAffidevit={};
-          candidateAffidevit.lawPresentCountAF = present.length;
-          if(candidateAffidevit.lawPresentCountAF) {
-            candidateAffidevit.lawPresentAF = present;
-          }
-          candidateAffidevit.lawPastCountAF = past.length;
-          if(candidateAffidevit.lawPastCountAF )
-            candidateAffidevit.lawPastAF = past;
-          //console.log(candidateAffidevit);
+          candidateAffidevit.totalLoanAF = totalLoan;
+          candidateAffidevit.loanAF = loanAF;
 
+          //console.log(candidateAffidevit);
 
           app.models.candidate.updateAll({oldAffidevitId: eachRow.profile_id}, candidateAffidevit, function(errr, info) {
             if(errr)
