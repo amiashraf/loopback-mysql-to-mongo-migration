eebb8c2ddcadc19c0ea0a834683886565fcc2b9e
Estiak ahamed <aiubpolin@gmail.com>
2015-12-27 19:46:19 +0600

    updated model and migration script of election seat

--- common/models/current-election.js
+++ common/models/current-election.js
@@ -1,3 +1,56 @@
+var Promise = require('bluebird');
 module.exports = function(CurrentElection) {
+  CurrentElection.getCurrentElectionsWithEelection = function(cb) {
+    //var currentDate = new Date();
+    //var currentHour = currentDate.getHours();
+    //var OPEN_HOUR = 6;
+    //var CLOSE_HOUR = 20;
+    //console.log('Current hour is ' + currentHour);
+    //var response;
+    //if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+    //  response = 'We are open for business.';
+    //} else {
+    //  response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+    //}
+    //cb(null, response);
+    var response = [];
+    CurrentElection.find({include:'election'}, function(err, currentElections) {
+      response = currentElections;
+      console.log(response);
+      //currentElections.forEach(currentEllecton);
+      //var promises = [];
+      //for(var currentElection in currentElections){
+      //  //promises.push(CurrentElection.election(currentElection));
+      //  CurrentElection.election(console.log);
+      //}
 
+      cb(null, response);
+    });
+      //.election(function(err, election){
+      //console.log(election);
+    //});
+
+    //Promise.all(promises).then(function (datas) {
+    //  log('Creating fake data done!');
+    //  log(datas);
+    //}).catch();
+
+    //var response = CurrentElection.find({
+    //  filter: {
+    //    order: 'created DESC',
+    //    include: [
+    //      'electionCandidatePosts'
+    //    ]
+    //  }
+    //});
+
+
+  };
+  CurrentElection.remoteMethod(
+    'getCurrentElectionsWithEelection',
+    {
+      http: {path: '/getCurrentElectionsWithEelection', verb: 'get'},
+      returns: {arg: 'data', type: 'Array'}
+    }
+  );
 };
